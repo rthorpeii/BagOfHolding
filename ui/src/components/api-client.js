bff9ccba2da9918bcf22383cf37010d61734efa9
rthorpeii <robertthorpe.ii@gmail.com>
2021-02-06 23:10:01 -0500

    Updating built ui

--- ui/src/components/api-client.js
+++ ui/src/components/api-client.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 export const ApiClient = axios.create({
   // Production
-  // baseURL: 'https://handy-haversack.herokuapp.com/api/',
+  baseURL: 'https://handy-haversack.herokuapp.com/api/',
   
   // Development
-  baseURL: 'http://localhost:8080/api/',
+  // baseURL: 'http://localhost:8080/api/',
 });
 
 export default ApiClient
