6fe98e12b4e2000b65dc4782c0ad7e8cc28f8cd5
rthorpeii <robertthorpe.ii@gmail.com>
2021-03-02 20:25:24 -0500

    Add environment variable for gorm loglevel

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"os"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -11,8 +13,21 @@ var DB *gorm.DB
 
 // ConnectDataBase creates a connection to our database
 func ConnectDataBase() {
+	audience := os.Getenv("GORM_LOGGING")
+	var logLevel logger.LogLevel
+	switch audience {
+	case "error":
+		logLevel = logger.Error
+	case "warn":
+		logLevel = logger.Warn
+	case "silent":
+		logLevel = logger.Silent
+	default:
+		logLevel = logger.Info
+	}
+
 	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
+		Logger: logger.Default.LogMode(logLevel),
 	})
 
 	if err != nil {
